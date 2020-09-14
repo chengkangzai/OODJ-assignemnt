84e4f2e05f12462df61f891eab0c9afb223a7aa8
chengkangzai <pycck@hotmail.com>
2020-09-14 23:23:32 +0800

    Initial commit

---
+++ OODJ/src/oodj/OODJ.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oodj;
+
+/**
+ *
+ * @author CCK
+ */
+public class OODJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
