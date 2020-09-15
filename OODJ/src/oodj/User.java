345a1bc8598bca8aacc13ffc310112a61abe8137
chengkangzai <pycck@hotmail.com>
2020-09-15 10:33:43 +0800

    add Javadov

--- OODJ/src/oodj/User.java
+++ OODJ/src/oodj/User.java
@@ -29,17 +29,16 @@ public class User {
     private int id;
     private boolean authenticated;
 
-    private static String FILENAME = "user.txt";
-    private static Path PATH = Paths.get(FILENAME);
+    private final static String FILENAME = "user.txt";
+    private final static Path PATH = Paths.get(FILENAME);
 
     public User() {
         this.authenticated = false;
     }
 
-    public User(String name, int id, String email) {
+    public User(String name, String email) {
         this.authenticated = false;
         this.name = name;
-        this.id = id;
         this.email = email;
     }
 
@@ -87,6 +86,14 @@ public class User {
         return false;
     }
 
+    /**
+     *
+     * Register the user by object and password as string
+     * 
+     * @param user
+     * @param password
+     * @return
+     */
     public boolean register(User user, String password) {
         if (isValidEmail(user.email) && isValidString(user.name)) {
 
@@ -101,9 +108,36 @@ public class User {
         }
         return false;
     }
+    
+    public boolean update(User user) {
+        //TODO
+        List<String> fromFile = getFromFile();
+        String[] data = new String[fromFile.size()];
+
+        System.out.println(user.id);
+        
+//        int idd = Integer.valueOf(userid);
+//        String line = data[idd];
+//        String[] splited = line.split("\\,");
+//        String stock = splited[3];
+//        int newStock = (Integer.valueOf(stock) - 1);
+//
+//        data[idd] = splited[0] + "," + splited[1] + "," + splited[2] + "," + String.valueOf(newStock) + "," + splited[4]
+//                + "," + splited[5];
+//
+//        String info = "";
+//        for (int i = 0; i < data.length; i++) {
+//            info += data[i] + "\n";
+//        }
+        
+        
+        
+        return false;
+    }
+
 
     /**
-     *
+     * Get the hashed value of the input Bytes
      * https://www.tutorialspoint.com/java_cryptography/java_cryptography_message_digest.htm
      *
      * @param inputBytes
@@ -123,10 +157,24 @@ public class User {
         return hashValue;
     }
 
+    /**
+     *
+     * Validate the string is valid or not
+     *
+     * @param String input
+     * @return true is the string is not empty and not containing coma(,)
+     */
     private static boolean isValidString(String input) {
         return !(input.isEmpty() || input.matches(","));
     }
 
+    /**
+     *
+     * Validate the email is valid or not
+     * 
+     * @param String email
+     * @return true if the input string is not empty and is a email
+     */
     private static boolean isValidEmail(String email) {
         if (!isValidString(email)) {
             return false;
@@ -136,14 +184,32 @@ public class User {
         return pat.matcher(email).matches();
     }
 
+    /**
+     * 
+     * Validate if the user is authenticate or not
+     *
+     * @return true if the user is authenticated
+     */
     public boolean isAuthenticated() {
         return this.authenticated;
     }
 
+    /**
+     *
+     * Validated is the user is admin or not
+     * 
+     * @return true if the user is admin
+     */
     public boolean isAdmin() {
         return this.isAuthenticated() && "admin".equals(this.role);
     }
 
+    /**
+     * 
+     * Get all the line from the text file
+     *
+     * @return Raw data from the text file
+     */
     private static List<String> getFromFile() {
         List<String> data = null;
         try {
@@ -159,7 +225,7 @@ public class User {
      * https://rollbar.com/guides/java-throwing-exceptions/
      *
      * @param arraylist
-     * @return
+     * @return List<String>
      * @throws Exception
      */
     private String listToString(List<String> arraylist) {
@@ -172,7 +238,14 @@ public class User {
         return line;
     }
 
-    public boolean reWriteDB(String data) {
+    /**
+     *
+     * Rewrite the text file, make sure the line is properly formatted
+     * 
+     * @param data
+     * @return
+     */
+    private boolean reWriteDB(String data) {
         boolean success = false;
         try {
             Files.write(PATH, data.getBytes(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
